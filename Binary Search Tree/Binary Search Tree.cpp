7beeed40c7f042167caed6d8573e5b5c71eb8848
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-29 12:23:57 +0700

    memanggil fungsi utama

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -130,6 +130,10 @@ int main()
 		cout << "4. Perform postorder traversal" << endl; 
 		cout << "5. Exit" << endl; 
 		cout << "\nEnter your choice (1-5) : "; 
+
+		char ch;
+		cin >> ch;
+		cout << endl;
 	}
 }
 
