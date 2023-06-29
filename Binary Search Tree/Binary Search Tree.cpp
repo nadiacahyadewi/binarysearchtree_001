7b9c21d79c110a8200b207ddaca80c9419602661
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-29 11:24:04 +0700

    jika nilai node lebih kecil

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -45,6 +45,11 @@ public:
 			return; // Exit
 		}
 
+		if (element < parent->info) // If the value in the data field of the new node is less than that of the parent
+		{
+			parent->leftchild = newNode; // Make the left child of the parent point to the new node
+		}
+		
 	}
 
 };
