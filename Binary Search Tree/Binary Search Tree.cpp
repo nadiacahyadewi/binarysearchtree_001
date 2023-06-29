0bc4e32872b74f2117a482fafda5bc36526ac8cd
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-29 12:28:57 +0700

    menambahkan perintah pada case 1

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -134,6 +134,18 @@ int main()
 		char ch;
 		cin >> ch;
 		cout << endl;
+
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		}
 	}
 }
 
